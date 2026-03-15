d4acefc339b91800068e1c76c6fa80076f099289
Amos M. Abdallah <amgenious28@gmail.com>
2026-03-15 20:32:52 +0300

    Update db.sql

---
+++ Animated login page/db.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`admin_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
